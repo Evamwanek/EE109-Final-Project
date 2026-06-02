f2947af6bc8f852994aecc7ce0573cd0f877e628
Ubuntu <ubuntu@ip-172-31-40-27.ec2.internal>
2026-06-02 19:28:08 +0000

    restored full FFT kernel, works

--- FFTInferenceHLS/src/fft_kernel.cpp
+++ FFTInferenceHLS/src/fft_kernel.cpp
@@ -32,103 +32,95 @@ void fft_kernel(
 #pragma HLS INTERFACE s_axilite port=output_imag bundle=control
 #pragma HLS INTERFACE ap_ctrl_hs port=return
 
+
+    fixed_t real[N];
+    fixed_t imag[N];
+    fixed_t tmp_real[N];
+    fixed_t tmp_imag[N];
+    fixed_t out_real[N];
+    fixed_t out_imag[N];
+
+#pragma HLS BIND_STORAGE variable=real type=ram_2p impl=bram
+#pragma HLS BIND_STORAGE variable=imag type=ram_2p impl=bram
+#pragma HLS BIND_STORAGE variable=tmp_real type=ram_2p impl=bram
+#pragma HLS BIND_STORAGE variable=tmp_imag type=ram_2p impl=bram
+#pragma HLS BIND_STORAGE variable=out_real type=ram_2p impl=bram
+#pragma HLS BIND_STORAGE variable=out_imag type=ram_2p impl=bram
+
     for (int f = 0; f < NUM_FRAMES; f++) {
-    for (int i = 0; i < N; i++) {
-        #pragma HLS PIPELINE II=1
-        output_real[f * N + i] = hamming[i];
-        output_imag[f * N + i] = 0;
+
+        for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
+            real[i] = input_real[f * N + i];
+            imag[i] = input_imag[f * N + i];
+        }
+
+        for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
+            fixed_t w = hamming[i];
+            real[i] = (real[i] * w) >> FIXED_SHIFT;
+            imag[i] = (imag[i] * w) >> FIXED_SHIFT;
+        }
+
+        for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
+            int rev = bit_reverse(i);
+            tmp_real[rev] = real[i];
+            tmp_imag[rev] = imag[i];
+        }
+
+        for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
+            real[i] = tmp_real[i];
+            imag[i] = tmp_imag[i];
+        }
+
+        for (int s = 0; s < NUM_STAGES; s++) {
+            int half_stride = 1 << s;
+            int stride = 1 << (s + 1);
+            int tw_step = 1 << (NUM_STAGES - 1 - s);
+
+            for (int k = 0; k < 512; k++) {
+#pragma HLS PIPELINE II=1
+                int group = k >> s;
+                int pos = k & (half_stride - 1);
+
+                int top = group * stride + pos;
+                int bot = top + half_stride;
+                int tw_idx = pos * tw_step;
+
+                fixed_t w_real = twR[tw_idx];
+                fixed_t w_imag = twI[tw_idx];
+
+                fixed_t a_real = real[top];
+                fixed_t a_imag = imag[top];
+                fixed_t b_real = real[bot];
+                fixed_t b_imag = imag[bot];
+
+                fixed_t bw_real =
+                    ((b_real * w_real) - (b_imag * w_imag)) >> FIXED_SHIFT;
+
+                fixed_t bw_imag =
+                    ((b_real * w_imag) + (b_imag * w_real)) >> FIXED_SHIFT;
+
+                out_real[top] = a_real + bw_real;
+                out_imag[top] = a_imag + bw_imag;
+                out_real[bot] = a_real - bw_real;
+                out_imag[bot] = a_imag - bw_imag;
+            }
+
+            for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
+                real[i] = out_real[i];
+                imag[i] = out_imag[i];
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+#pragma HLS PIPELINE II=1
+            output_real[f * N + i] = real[i];
+            output_imag[f * N + i] = imag[i];
+        }
+    }
 }
 }
- }}
-
-//     fixed_t real[N];
-//     fixed_t imag[N];
-//     fixed_t tmp_real[N];
-//     fixed_t tmp_imag[N];
-//     fixed_t out_real[N];
-//     fixed_t out_imag[N];
-
-// #pragma HLS BIND_STORAGE variable=real type=ram_2p impl=bram
-// #pragma HLS BIND_STORAGE variable=imag type=ram_2p impl=bram
-// #pragma HLS BIND_STORAGE variable=tmp_real type=ram_2p impl=bram
-// #pragma HLS BIND_STORAGE variable=tmp_imag type=ram_2p impl=bram
-// #pragma HLS BIND_STORAGE variable=out_real type=ram_2p impl=bram
-// #pragma HLS BIND_STORAGE variable=out_imag type=ram_2p impl=bram
-
-//     for (int f = 0; f < NUM_FRAMES; f++) {
-
-//         for (int i = 0; i < N; i++) {
-// #pragma HLS PIPELINE II=1
-//             real[i] = input_real[f * N + i];
-//             imag[i] = input_imag[f * N + i];
-//         }
-
-//         for (int i = 0; i < N; i++) {
-// #pragma HLS PIPELINE II=1
-//             fixed_t w = hamming[i];
-//             real[i] = (real[i] * w) >> FIXED_SHIFT;
-//             imag[i] = (imag[i] * w) >> FIXED_SHIFT;
-//         }
-
-//         for (int i = 0; i < N; i++) {
-// #pragma HLS PIPELINE II=1
-//             int rev = bit_reverse(i);
-//             tmp_real[rev] = real[i];
-//             tmp_imag[rev] = imag[i];
-//         }
-
-//         for (int i = 0; i < N; i++) {
-// #pragma HLS PIPELINE II=1
-//             real[i] = tmp_real[i];
-//             imag[i] = tmp_imag[i];
-//         }
-
-//         for (int s = 0; s < NUM_STAGES; s++) {
-//             int half_stride = 1 << s;
-//             int stride = 1 << (s + 1);
-//             int tw_step = 1 << (NUM_STAGES - 1 - s);
-
-//             for (int k = 0; k < 512; k++) {
-// #pragma HLS PIPELINE II=1
-//                 int group = k >> s;
-//                 int pos = k & (half_stride - 1);
-
-//                 int top = group * stride + pos;
-//                 int bot = top + half_stride;
-//                 int tw_idx = pos * tw_step;
-
-//                 fixed_t w_real = twR[tw_idx];
-//                 fixed_t w_imag = twI[tw_idx];
-
-//                 fixed_t a_real = real[top];
-//                 fixed_t a_imag = imag[top];
-//                 fixed_t b_real = real[bot];
-//                 fixed_t b_imag = imag[bot];
-
-//                 fixed_t bw_real =
-//                     ((b_real * w_real) - (b_imag * w_imag)) >> FIXED_SHIFT;
-
-//                 fixed_t bw_imag =
-//                     ((b_real * w_imag) + (b_imag * w_real)) >> FIXED_SHIFT;
-
-//                 out_real[top] = a_real + bw_real;
-//                 out_imag[top] = a_imag + bw_imag;
-//                 out_real[bot] = a_real - bw_real;
-//                 out_imag[bot] = a_imag - bw_imag;
-//             }
-
-//             for (int i = 0; i < N; i++) {
-// #pragma HLS PIPELINE II=1
-//                 real[i] = out_real[i];
-//                 imag[i] = out_imag[i];
-//             }
-//         }
-
-//         for (int i = 0; i < N; i++) {
-// #pragma HLS PIPELINE II=1
-//             output_real[f * N + i] = real[i];
-//             output_imag[f * N + i] = imag[i];
-//         }
-//     }
-// }
-//}
